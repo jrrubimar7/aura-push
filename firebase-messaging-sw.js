10a41fdc90b13a1e8afa2be0f98c08bdd84bf740
jrrubimar7 <raysinternational999@gmail.com>
2026-03-29 13:22:30 +0200

    Create firebase-messaging-sw.js

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,12 @@
+importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBjKF1RgTjQKpahYkoff5T0xmuDoM-84wY",
+  authDomain: "aura-46bb4.firebaseapp.com",
+  projectId: "aura-46bb4",
+  messagingSenderId: "222284414509",
+  appId: "1:222284414509:web:9739cb173f5a78de9382a4"
+});
+
+firebase.messaging();
